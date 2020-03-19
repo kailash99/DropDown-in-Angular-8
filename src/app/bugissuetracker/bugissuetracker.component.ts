092a3fab7e4dae0c8be6a6f183b32d9edaef3072
kailash999 <kailas.j@sankeysolutions.com>
2020-03-19 19:04:00 +0530

    commit H3

--- src/app/bugissuetracker/bugissuetracker.component.ts
+++ src/app/bugissuetracker/bugissuetracker.component.ts
@@ -6,33 +6,124 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./bugissuetracker.component.css']
 })
 export class BugissuetrackerComponent implements OnInit {
-  name = 'Angular 6';
-
  
-  selected= {
-    name: 'done',
-    color: 'green'
-  }
+  headers = ["ID", "Description"];
+  rows = [
+    {
+      "ID" : "1",
+      "Description" :"Create login page",
+    },
+    {
+      "ID" : "2",
+      "Description" :"Create rgistration page",
+    },
+    {
+      "ID" : "3",
+      "Description" :"Integrate OTP API with registration page",
+    }
+    
+  ]
+  selectedColor = '';
 
-  data = [{
-    name: 'Done',
-    color: 'green'
-  }, {
-    name: 'Not Started',
-    color: 'red'
-  }, {
-    name: 'In Progress',
-    color: 'blue'
-  }]
-  // onChange(value){
-  //   this.selectedColor = value;
-  // }
-  compareObjects(o1: any, o2: any): boolean {
-    return o1.color === o2.color
-  }
+  status = [
+    {
+      name: 'Done',
+      value: '#ffff00'
+    },
+    {
+      name: 'In Progress',
+      value: '#ff3300'
+    },
+    {
+      name: 'Not Started',
+      value: '#0000ff'
+    }
+  ];
 
+  onChange(value){
+    this.selectedColor = value;
+  }
   ngOnInit() {
-    // this.selectedColor = this.selectedColor[0].name;
   }
+
+  textBoxClick: boolean = true;
+  enable(){
+    this.textBoxClick = false
+  }
+  disable(){
+    this.textBoxClick = true
+  }
+
 
 }
+
+  // selectedColor: any = {
+  //   value: []
+  // }
+
+  // color: any ={
+  //   col: [
+  //     {
+  //       cName: 'done',
+  //       color: 'green'
+  //     },
+  //     {
+  //       cName: 'in progress',
+  //       color: 'blue'
+  //     },
+  //     {
+  //       cName: 'start work',
+  //       color: 'red'
+  //     }
+  //   ]
+  // }
+  // selected: any = {
+  //   value: []
+  // }
+
+  // color: any ={
+  //   col: [
+  //     {
+  //       cName: 1,
+  //       color: 'green'
+  //     },
+  //     {
+  //       cName: 2,
+  //       color: 'blue'
+  //     },
+  //     {
+  //       cName: 3,
+  //       color: 'red'
+  //     }
+  //   ]
+  // }
+  
+  // onChangeSelectedDetails(event, id, o1){
+  //   console.log(this.selected.value[id]);
+  //   return o1.color
+  // }
+
+ 
+  // selected= {
+  //   name: 'done',
+  //   color: 'green'
+  // }
+
+  // data = [
+  //   {
+  //   name: 'Done',
+  //   color: 'green'
+  //   },
+  //  {
+  //   name: 'Not Started',
+  //   color: 'red'
+  //  },
+  //  {
+  //   name: 'In Progress',
+  //   color: 'blue'
+  //  }]
+  // // 
+  // compareObjects(o1: any, o2: any): boolean {
+  //   return o1.color === o2.color
+  // }
+ 
